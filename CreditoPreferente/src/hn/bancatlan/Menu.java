443cdfa998036514689cff07f95e5d2454eba396
azuniga013 <adanzuniga@gmail.com>
2015-04-17 14:59:34 -0600

    Actualizacion Credito Preferente

---
+++ CreditoPreferente/src/hn/bancatlan/Menu.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hn.bancatlan;
+import java.io.*;
+        
+/**
+ *
+ * @author azuniga
+ * @version 1
+ */
+public class Menu {
+   
+    public void menu()
+    {
+        System.out.print("Hola");
+    }
+    }
+   
+}
