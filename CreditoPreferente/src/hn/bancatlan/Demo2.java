3888f819cb3df9c26d6973b1a3cc920c707cfff9
azuniga013 <adanzuniga@gmail.com>
2015-04-17 16:09:42 -0600

    Actualizacion Credito Preferente5

---
+++ CreditoPreferente/src/hn/bancatlan/Demo2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hn.bancatlan;
+
+/**
+ *
+ * @author azuniga
+ */
+public class Demo2 {
+    public void Demo2() {
+    
+}
+}
